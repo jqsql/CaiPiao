adbecc00d91a701896445e8ae0df75f8d0bb0146
jqs <1517908593@qq.com>
2018-04-24 18:25:16 +0800

    上传自定义控件布局
    2018.4.24 18:00

--- CaiPiao/app/src/main/java/com/jqscp/MainActivity.java
+++ CaiPiao/app/src/main/java/com/jqscp/MainActivity.java
@@ -2,12 +2,81 @@ package com.jqscp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+import com.jqscp.View.CircleBGTextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<Integer> mList= new ArrayList<>();
+    GridView m01;
+    GridView m02;
+    GridView m03;
+    GridView m04;
+    GridView m05;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mList= Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+        m01=findViewById(R.id.GridView01);
+        m02=findViewById(R.id.GridView02);
+        m03=findViewById(R.id.GridView03);
+        m04=findViewById(R.id.GridView04);
+        m05=findViewById(R.id.GridView05);
+        m01.setAdapter(new GridViewAdapter());
+        m02.setAdapter(new GridViewAdapter());
+        m03.setAdapter(new GridViewAdapter());
+        m04.setAdapter(new GridViewAdapter());
+        m05.setAdapter(new GridViewAdapter());
+    }
+
+
+
+    class GridViewAdapter extends BaseAdapter{
+        public GridViewAdapter() {
+
+        }
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return mList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            viewHolder mViewHolder;
+            if(view==null){
+                mViewHolder=new viewHolder();
+                view= LayoutInflater.from(MainActivity.this).inflate(R.layout.circle_bg_item,null);
+                mViewHolder.mTextView=view.findViewById(R.id.text);
+                view.setTag(mViewHolder);
+            }else {
+                mViewHolder= (viewHolder) view.getTag();
+            }
+            mViewHolder.mTextView.setText(mList.get(i)+"");
+            return view;
+        }
+        class viewHolder{
+            CircleBGTextView mTextView;
+        }
     }
 }
---
+++ CaiPiao/app/src/main/java/com/jqscp/View/CircleBGTextView.java
@@ -0,0 +1,128 @@
+package com.jqscp.View;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import com.jqscp.R;
+
+/**
+ * 圆形背景的自定义view
+ */
+
+public class CircleBGTextView extends AppCompatTextView{
+    private Context mContext;
+    /**
+     * 画笔
+     */
+    private Paint mPaint;
+    /**
+     * 圆的边框颜色
+     */
+    private int mBorderColor;
+    /**
+     * 圆的边框宽度
+     */
+    private int mBorderSize;
+    /**
+     * 圆的填充颜色
+     */
+    private int mFillColor;
+    /**
+     * 是否填充颜色
+     */
+    private boolean isFillColor;
+    public CircleBGTextView(Context context) {
+        this(context,null);
+    }
+
+    public CircleBGTextView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public CircleBGTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.mContext=context;
+        this.setGravity(Gravity.CENTER);
+        mPaint = new Paint();
+        //获取自定义属性值
+        TypedArray mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleBGTextStyle);
+        mBorderColor = mTypedArray.getColor(R.styleable.CircleBGTextStyle_borderColor, getResources().getColor(R.color.Grey));
+        mFillColor = mTypedArray.getColor(R.styleable.CircleBGTextStyle_fillColor, getResources().getColor(R.color.Red));
+        mBorderSize = mTypedArray.getDimensionPixelSize(R.styleable.CircleBGTextStyle_borderColor, 2);
+        isFillColor = mTypedArray.getBoolean(R.styleable.CircleBGTextStyle_isFillColor, false);
+        mTypedArray.recycle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //开始绘制 内部实心圆
+        mPaint.setAntiAlias(true);//消除锯齿
+        if (isFillColor) {//判断是否绘画内部实心圆
+            this.setTextColor(Color.WHITE);
+            mPaint.setColor(mFillColor);//画笔颜色
+            mPaint.setStyle(Paint.Style.FILL);//实心
+        }else {
+            this.setTextColor(Color.RED);
+            mPaint.setColor(mBorderColor);//画笔颜色
+            mPaint.setStyle(Paint.Style.STROKE);//空心
+            mPaint.setStrokeWidth(mBorderSize);
+        }
+        //创建一个区域,限制圆弧范围
+        RectF rectF = new RectF();
+        //设置半径,比较长宽,取最大值
+        int radius = getWidth() > getHeight() ? getWidth() : getHeight();
+        ViewGroup.LayoutParams params= CircleBGTextView.this.getLayoutParams();
+        params.width=radius;
+        params.height=radius;
+        CircleBGTextView.this.setLayoutParams(params);
+        //设置Padding 不一致,绘制出的是椭圆;一致的是圆形
+        rectF.set(0,0,radius,radius);
+        //绘制圆弧
+        canvas.drawArc(rectF,0,360,false,mPaint);
+        super.onDraw(canvas);
+    }
+
+    public boolean isFillColor() {
+        return isFillColor;
+    }
+
+    public synchronized void setFillColor(boolean fillColor) {
+        isFillColor = fillColor;
+        invalidate();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                if (x + getLeft() < getRight() && y + getTop() < getBottom()) {
+                    if(isFillColor){
+                        setFillColor(false);
+                    }else {
+                        setFillColor(true);
+                    }
+                }
+                break;
+        }
+        return true;
+    }
+
+
+}
